e9983d232a5dba235ee9cdf02b9a337f9899415a
cassonchris <cassonchris723@gmail.com>
2020-06-26 12:22:47 -0500

    Set domain and clientId in frontend

--- client/src/config.ts
+++ client/src/config.ts
@@ -3,8 +3,7 @@ const apiId = '...'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-58bkvb9a.us.auth0.com', // Auth0 domain
+  clientId: 'ymg7l36U3i5Hx8XK7v0YVqeJFXFC1Oq1', // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
