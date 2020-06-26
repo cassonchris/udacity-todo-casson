3b4bc7d69756f35b2820654d0da3608cb4eba583
cassonchris <cassonchris723@gmail.com>
2020-06-26 13:09:10 -0500

    Implement getTodos

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,59 @@
 import 'source-map-support/register'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { getUserId } from '../utils'
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+import * as AWS from 'aws-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+const XAWS = AWSXRay.captureAWS(AWS)
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
+const docClient = new XAWS.DynamoDB.DocumentClient()
+
+const todosTable = process.env.TODOS_TABLE
+
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const userId = getUserId(event)
+    if (!userId) {
+      const error = 'failed to obtain userId'
+      console.error(`getTodos.handler -> ${error}`)
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          error
+        })
+      }
+    }
+
+    const todos = await getTodosByUserId(userId)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        items: todos
+      })
+    }
+  }
+)
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
+
+async function getTodosByUserId(userId: string) {
+  const result = await docClient
+    .query({
+      TableName: todosTable,
+      KeyConditionExpression: 'userId = :userId',
+      ExpressionAttributeValues: {
+        ':userId': userId
+      },
+      ScanIndexForward: false
+    })
+    .promise()
+
+  return result.Items
 }
